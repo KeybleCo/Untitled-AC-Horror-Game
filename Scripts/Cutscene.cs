4e2fddaeb5afb37f54bbe7fcbffe81809b17f058
Colin Pham <SettingFire666@gmail.com>
2025-12-19 00:21:33 -0800

    Proof of Concept Basics Done

---
+++ Scripts/Cutscene.cs
@@ -0,0 +1,91 @@
+using Godot;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public partial class Cutscene : VideoStreamPlayer
+{
+    bool canControl = false;
+    public override async void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        Position = Vector2.Zero;
+        await Task.Delay(2300);
+        Paused = true;
+        canControl = true;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion motion)
+        {
+            float ifX = Position.X - motion.Relative.X;
+            float ifY = Position.Y - motion.Relative.Y;
+
+            float newX;
+            float newY;
+            if (ifX >= -16 && ifX <= 16)
+            {
+                newX = (float) (Position.X - (motion.Relative.X * 0.5));
+            } else if (ifX >= -24 && ifX <= 24)
+            {
+                newX = (float) (Position.X - (motion.Relative.X * 0.2));
+            } else if (ifX >= -32 && ifX <= 32)
+            {
+                newX = (float) (Position.X - (motion.Relative.X * 0.05));
+            } else
+            {
+                newX = Position.X;
+            }
+
+            if (ifY >= -12 && ifY <= 12)
+            {
+                newY = (float) (Position.Y - (motion.Relative.Y * 0.5));
+            } else if (ifY >= -18 && ifY <= 18)
+            {
+                newY = (float) (Position.Y - (motion.Relative.Y * 0.2));
+            } else if (ifY >= -24 && ifY <= 24)
+            {
+                newY = (float) (Position.Y - (motion.Relative.Y * 0.05));
+            } else
+            {
+                newY = Position.Y;
+            }
+
+            Position = new Vector2(newX, newY);
+        }
+
+        if (Input.IsActionJustPressed("Walk"))
+        {
+            if (canControl)
+            {
+                Paused = false;   
+            }
+        }
+
+        if (Input.IsActionJustReleased("Walk"))
+        {
+            if (canControl)
+            {
+                Paused = true;   
+            }
+        }
+
+        if (Input.IsActionJustPressed("Run"))
+        {
+            if (canControl)
+            {
+                SpeedScale = 1.5f;  
+            }
+        }
+
+        if (Input.IsActionJustReleased("Run"))
+        {
+            if (canControl)
+            {
+                SpeedScale = 1.0f;  
+            }
+        }
+    }
+
+}
